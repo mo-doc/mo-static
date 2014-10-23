afbe7758de1a293ca8452f793d45899149d7f8d1
yyy <super.doter.yyy@gmail.com>
2014-10-23 21:54:59 +0800

    fix

---
+++ app/dep/filter/classify.js
@@ -0,0 +1,13 @@
+// MOapp.filter("classify", function() {
+//     return function(value) {
+//         value = value +"";
+//         switch(value){
+//         	case "base":
+//         		return "基础组件";
+//     		case "util":
+//     			return "组件";
+// 			case "server":
+// 				return "服务";
+//         }
+//     };
+// });
